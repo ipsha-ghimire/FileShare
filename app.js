a623d3a071ff1239dd619ee0bba18e7a700768d6
Ipsha Ghimire <ipshagh2000@gmail.com>
2022-12-07 19:02:21 +0545

    connected to mongo

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express= require("express");
+const multer= require('multer');
+const mongoose= require("mongoose")
+require("dotenv").config();
 const app= express();
+mongoose.connect(process.env.DATABASE_URL);
+const upload= multer({dest:"uploads"}); //a middleware
 
 app.set("view engine","ejs");
-app.use(express.static("public"))
-
+app.use(express.static("public"));
 app.get("/",(req,res)=>{
     res.render("index");
 });
-app.listen(3000,(req,res)=>{
-    console.log("app listening on port 3000");
+
+app.post("/upload",upload.array("file"),(req,res)=>{
+res.send('hi');
 })
+app.listen(process.env.PORT,(req,res)=>{
+  console.log("app listening on port 3000");
+})
+
