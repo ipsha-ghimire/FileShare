b623eacc78192106fa8d654feeec45b3fd8e82e2
Ipsha Ghimire <ipshagh2000@gmail.com>
2022-12-09 18:20:40 +0545

    download file completed

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
-const express= require("express");
-const multer= require('multer');
-const mongoose= require("mongoose")
+const express = require("express");
+const multer = require("multer")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/File")
 require("dotenv").config();
-const app= express();
-mongoose.connect(process.env.DATABASE_URL);
-const upload= multer({dest:"uploads"}); //a middleware
+const app = express();
+app.use(express.urlencoded({ extended: true }))
 
-app.set("view engine","ejs");
+const upload = multer({ dest: "uploads" }) //this is now a middleware
+mongoose.connect(process.env.DATABASE_URL);
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.get("/",(req,res)=>{
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post("/upload",upload.array("file"),(req,res)=>{
-res.send('hi');
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalname: req.file.originalname
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+  console.log(file);
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+
 })
-app.listen(process.env.PORT,(req,res)=>{
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+if( !(await bcrypt.compare(req.body.password,file.password))){
+res.render("password",{error:true})
+return 
+}
+  }
+
+file.downloadCount++
+await file.save();
+console.log(file.downloadCount)
+res.download(file.path,file.originalname);
+
+}
+
+app.listen(process.env.PORT, (req, res) => {
   console.log("app listening on port 3000");
 })
 
